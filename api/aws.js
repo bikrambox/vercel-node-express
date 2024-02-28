9830a1416b70cf8e1886501d3f53ff03815e314b
Bikram rongpi <bikramrongpi088@gmail.com>
2024-02-28 13:25:05 +0530

    Initial commit

--- api/aws.js
+++ api/aws.js
@@ -10,7 +10,7 @@ const AWS = require('aws-sdk');
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION
+  region: 'eu-central-1'
 });
 
 // Create an EC2 service object
