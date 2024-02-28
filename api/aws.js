e5340df5446479c987d627fa8b5a89b916134798
Bikram rongpi <bikramrongpi088@gmail.com>
2024-02-28 12:56:48 +0530

    Initial commit

--- api/aws.js
+++ api/aws.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello OtherWorld!');
+  res.send('Hello aws!');
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 const express = require('express');
 const app = express();
 const helloRoute = require('./api/hello');
+const awsRoute = require('./api/aws');
+
 const port = 3000;
 
-app.use('/api', helloRoute);
+app.use('/api/hello', helloRoute);
+app.use('/api/aws', awsRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
