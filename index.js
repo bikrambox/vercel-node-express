e31b79f37fe0b50eede090669303b3f45b3d5a5f
Bikram rongpi <bikramrongpi088@gmail.com>
2024-02-29 12:57:18 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // index.js
 const express = require('express');
 const path = require('path');
-// const port = 3000;
+const port = 3000;
 const app = express();
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,12 +13,12 @@ const awsRoute = require('./api/aws');
 app.use('/api/hello', helloRoute);
 app.use('/api/aws', awsRoute);
 
-// app.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-// });
-
-// Start the server
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+// // Start the server
+// const port = process.env.PORT || 3000;
+// server.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
